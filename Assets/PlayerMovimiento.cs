271f217e21b2c6f5e807ba7d06eb1d4f83c63fab
BrunoMendiburu <101568817+BrunoMendiburu@users.noreply.github.com>
2022-06-01 15:51:35 -0300

    MovimientoYCamara

---
+++ Assets/PlayerMovimiento.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovimiento : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float jumpForce;
+    public int maxJumps;
+
+    int hasJump;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasJump = maxJumps;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(-movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+    }
